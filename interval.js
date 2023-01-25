ef072f5befbf3018e2310369f7ebb0b06f8d6ade
wasdwert <102148004+wasdwert@users.noreply.github.com>
2023-01-25 15:31:47 +0100

    Add files via upload

--- interval.js
+++ interval.js
@@ -3,15 +3,18 @@ var lastinterval=0;
 var thisinterval=0;
 var interval=1000;
 
-lastinterval= Date.now();
+window.onload = function () {
+    lastinterval= Date.now();
+    startinterval();
+}
 
-function intervaltimer() {
+function startinterval() {
     setTimeout(function() { 
         thisinterval= Date.now();
         interval= intervall-(lastinterval-thisinterval+1000);
         lastinterval=thisinterval;
         console.log(interval);
         self.postMessage("Ping");
-        intervaltimer();
+        startinterval();
     }, interval)/*1 Sekunden Intervallfunktion*/
 }
