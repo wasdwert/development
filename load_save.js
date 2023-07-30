0d7c885c1975cc97c9675d8bb6f9e379f32f1f01
wasdwert <102148004+wasdwert@users.noreply.github.com>
2023-07-30 10:08:57 +0200

    Add files via upload

--- load_save.js
+++ load_save.js
@@ -12,55 +12,6 @@ window.onload = function () {
             $('#LoadGame').show();
         });  
     }
-
-    $('#positionn_batch').select2({
-            minimumResultsForSearch: Infinity
-        });
-    $('#menu').select2({
-            minimumResultsForSearch: Infinity
-        });
-    $('#menu_shopping').select2({
-            minimumResultsForSearch: Infinity
-        });
-    $('#neighborhood_time').select2({
-            minimumResultsForSearch: Infinity
-        });
-    $('#cityboundaries_time').select2({
-            minimumResultsForSearch: Infinity
-        });
-    
-    menu_change();
-    menu_shop_change();
-    
-    fetchimage_seedling ("a", seedling1);
-    fetchimage_seedling ("b", seedling2);
-    fetchimage_seedling ("c", seedling3);
-    fetchimage_seedling ("d", seedling4);
-    fetchimage_seedling ("e", seedling5);
-    fetchimage_seedling ("f", seedling6);
-    
-    //document.getElementById("skill_growing_nr").innerHTML = skills.growing+1;
-    //document.getElementById("skill_growing_level").innerHTML = skills.growing_level;
-    //document.getElementById("skill_growing_time").innerHTML = skillgrowingtime[skills.growing];
-    //document.getElementById("skill_shaping_nr").innerHTML = skills.shaping+1;
-    //document.getElementById("skill_shaping_level").innerHTML = skills.shaping_level;
-    //document.getElementById("skill_shaping_time").innerHTML = skillshapingtime[skills.shaping];
-    
-    //document.getElementById("skilllevel").innerHTML = skills.level;
-    //document.getElementById("skillexp_left").innerHTML = skills.exp_nextlevel;
-    //document.getElementById("money").innerHTML = resources.money;
-    //document.getElementById("organic_waste").innerHTML = resources.organic_waste;
-    //document.getElementById("compost").innerHTML = resources.compost;
-    //document.getElementById("bolt").innerHTML = resources.bolt;
-    //document.getElementById("shop_bolt").innerHTML = resources.bolt;
-    //document.getElementById("metal").innerHTML = resources.metal;
-    //document.getElementById("shop_metal").innerHTML = resources.metal;
-    //document.getElementById("wire").innerHTML = resources.wire;
-    //document.getElementById("shop_wire").innerHTML = resources.wire;
-    //document.getElementById("board").innerHTML = resources.board;
-    //document.getElementById("shop_board").innerHTML = resources.board;
-    
-    w.postMessage('Start');
 }
 
 /*1 Sekunden Intervallfunktion*/
@@ -180,17 +131,11 @@ setInterval(function Autosave() {
 }, 30000)
 
 function NewGame() {
-    $(document).ready(function(){
-        $('.startscreen').hide();
-        $('.game').show();
-    });  
+    startup(); 
 }
 
 function NewGame2() {
-    $(document).ready(function(){
-        $('.startscreen').hide();
-        $('.game').show();
-    });  
+    startup(); 
 }
 
 function LoadGame() {
@@ -207,10 +152,64 @@ function LoadGame() {
     seedling4 = JSON.parse(localStorage.getItem('seedling4'));
     seedling5 = JSON.parse(localStorage.getItem('seedling5'));
     seedling6 = JSON.parse(localStorage.getItem('seedling6'));
+    startup();
+    
+}
+
+function startup() {
     $(document).ready(function(){
         $('.startscreen').hide();
         $('.game').show();
     });  
+    
+    $('#positionn_batch').select2({
+            minimumResultsForSearch: Infinity
+        });
+    $('#menu').select2({
+            minimumResultsForSearch: Infinity
+        });
+    $('#menu_shopping').select2({
+            minimumResultsForSearch: Infinity
+        });
+    $('#neighborhood_time').select2({
+            minimumResultsForSearch: Infinity
+        });
+    $('#cityboundaries_time').select2({
+            minimumResultsForSearch: Infinity
+        });
+    
+    menu_change();
+    menu_shop_change();
+    
+    fetchimage_seedling ("a", seedling1);
+    fetchimage_seedling ("b", seedling2);
+    fetchimage_seedling ("c", seedling3);
+    fetchimage_seedling ("d", seedling4);
+    fetchimage_seedling ("e", seedling5);
+    fetchimage_seedling ("f", seedling6);
+    
+    //document.getElementById("skill_growing_nr").innerHTML = skills.growing+1;
+    //document.getElementById("skill_growing_level").innerHTML = skills.growing_level;
+    //document.getElementById("skill_growing_time").innerHTML = skillgrowingtime[skills.growing];
+    //document.getElementById("skill_shaping_nr").innerHTML = skills.shaping+1;
+    //document.getElementById("skill_shaping_level").innerHTML = skills.shaping_level;
+    //document.getElementById("skill_shaping_time").innerHTML = skillshapingtime[skills.shaping];
+    
+    //document.getElementById("skilllevel").innerHTML = skills.level;
+    //document.getElementById("skillexp_left").innerHTML = skills.exp_nextlevel;
+    //document.getElementById("money").innerHTML = resources.money;
+    //document.getElementById("organic_waste").innerHTML = resources.organic_waste;
+    //document.getElementById("compost").innerHTML = resources.compost;
+    //document.getElementById("bolt").innerHTML = resources.bolt;
+    //document.getElementById("shop_bolt").innerHTML = resources.bolt;
+    //document.getElementById("metal").innerHTML = resources.metal;
+    //document.getElementById("shop_metal").innerHTML = resources.metal;
+    //document.getElementById("wire").innerHTML = resources.wire;
+    //document.getElementById("shop_wire").innerHTML = resources.wire;
+    //document.getElementById("board").innerHTML = resources.board;
+    //document.getElementById("shop_board").innerHTML = resources.board;
+    
+    w.postMessage('Start');
 }
 
 function ViewOptions() {
